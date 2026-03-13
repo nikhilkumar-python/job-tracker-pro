03ff68a29798cde236dbd00001e02d4b5ba8a3ef
Nikhil kumar <kumarnikhil6722@gmail.com>
2026-03-13 11:06:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,12 +173,12 @@ def logout():
 
     return redirect("/login")          
 
-with app.app_context():
+    with app.app_context():
         db.create_all()
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-    
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
+
 
